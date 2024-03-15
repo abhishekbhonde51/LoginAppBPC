bf90429afa22eeaf33d2bad472059c64df68a3de
abhishekbhonde51 <abhishekbhonde_cs@jspmrscoe.edu.in>
2024-03-15 10:31:03 +0530

    added login app

--- loginapp/lib/main.dart
+++ loginapp/lib/main.dart
@@ -45,88 +45,89 @@ class _LoginAppState extends State{
               backgroundColor: Colors.blue,
           ),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMYoHcOPS-yV_FWtuRwCJIWXUQmxDfUCQSDAAidGEt98UQUo3PGbFO2upoF6m1LBJgVF0&usqp=CAU',
-                  height:200,
-                  width: 200,
-                  ),
-                  const SizedBox(
-                    height: 150,
-                  ),
-                  Form(
-                    child: TextFormField(
-                      key: formkey,
-                      controller: _userNameController,
-                      validator: (value){
+          child: Form(
+              key: formkey,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                    const SizedBox(
+                      height: 60,
+                    ),
+                    Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMYoHcOPS-yV_FWtuRwCJIWXUQmxDfUCQSDAAidGEt98UQUo3PGbFO2upoF6m1LBJgVF0&usqp=CAU',
+                    height:200,
+                    width: 200,
+                    ),
+                    const SizedBox(
+                      height: 150,
+                    ),
+                      TextFormField(
+                        controller: _userNameController,
+                        validator: (value){
+                          if(value==null || value.isEmpty){
+                            return "Please Enter Password";
+                          }
+                          else{
+                            return null;
+                          }
+                        },
+                        keyboardType:TextInputType.emailAddress,
+                        decoration:const InputDecoration(
+                          hintText: "Enter your email",
+                          labelText: "Email",
+                          prefixIcon: Icon(Icons.email),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10))
+                          )
+                        ),
+                      
+                    
+                    ),
+                    const SizedBox(height: 20,),
+                    TextFormField(
+                      controller: _passwordController,
+                      validator:(value){
                         if(value==null || value.isEmpty){
-                          return "Please Enter Password";
+                            return "Please Enter password";
                         }
                         else{
-                          return null;
+                            return null;
                         }
                       },
-                      keyboardType:TextInputType.emailAddress,
+                      keyboardType:TextInputType.visiblePassword,
+                      obscureText: true,
+                      obscuringCharacter: "*",
                       decoration:const InputDecoration(
-                        hintText: "Enter your email",
-                        labelText: "Email",
-                        prefixIcon: Icon(Icons.email),
+                        hintText: "Enter your password",
+                        labelText: "Password",
+                        prefixIcon: Icon(Icons.password_sharp),
+                        suffixIcon: Icon(Icons.remove_red_eye),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(10))
                         )
                       ),
-                    
                     ),
-                  ),
-                  const SizedBox(height: 20,),
-                  TextFormField(
-                    controller: _passwordController,
-                    validator:(value){
-                      if(value==null || value.isEmpty){
-                          return "Please Enter password";
-                      }
-                      else{
-                          return null;
-                      }
-                    },
-                    keyboardType:TextInputType.emailAddress,
-                    obscureText: true,
-                    obscuringCharacter: "*",
-                    decoration:const InputDecoration(
-                      hintText: "Enter your password",
-                      labelText: "Password",
-                      prefixIcon: Icon(Icons.password_sharp),
-                      suffixIcon: Icon(Icons.remove_red_eye),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10))
-                      )
+                    const SizedBox(
+                        height: 20,
                     ),
-                  ),
-                  const SizedBox(
-                      height: 20,
-                  ),
-                  ElevatedButton(onPressed: (){
-                    // bool userNameValidated = usernameKey.currentState!.validate();
-                    // bool formkey = passnameKey.currentState!.validate();
-
-                    if(formkey==true){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content:Text("Login Successful"))
-                      );
-                    }else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content:Text("Login failed"))
-                      );
-                    }
-                  }, child:const Text("Submit"))
-
-              ],
+                    ElevatedButton(onPressed: (){
+                      // bool userNameValidated = usernameKey.currentState!.validate();
+                      // bool formkey = passnameKey.currentState!.validate();
+                      bool loginValidated = formkey.currentState!.validate();
+                      if(loginValidated){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content:Text("Login Successful"))
+                        );
+                      }else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content:Text("Login failed"))
+                        );
+                      }
+                    }, child:const Text("Submit"))
+            
+                ],
+              ),
             ),
           ),
         ),
